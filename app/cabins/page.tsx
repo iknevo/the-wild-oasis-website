831a9904f63fc4bb2422cc1d096f6ff4c4e5182d
idknevo <nevoexists@gmail.com>
2025-06-23 12:34:28 +0300

    initial

--- app/cabins/page.tsx
+++ app/cabins/page.tsx
@@ -3,7 +3,7 @@ import { NextPage } from "next";
 interface Props {}
 
 const Page: NextPage<Props> = ({}) => {
-  return <div>page</div>;
+  return <div>cabins page</div>;
 };
 
 export default Page;
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -3,7 +3,7 @@ import { NextPage } from "next";
 interface Props {}
 
 const Page: NextPage<Props> = ({}) => {
-  return <div>page</div>;
+  return <div>login</div>;
 };
 
 export default Page;
