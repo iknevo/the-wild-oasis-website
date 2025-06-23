3b4da192b31ee21dfe72fcbe9dd4934bff995361
idknevo <nevoexists@gmail.com>
2025-06-23 13:34:16 +0300

    some files

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,16 @@
 import { NextPage } from "next";
+import Link from "next/link";
+import Navigation from "./components/Navigation";
 
 interface Props {}
 
 const Page: NextPage<Props> = ({}) => {
-  return <div>page</div>;
+  return (
+    <div>
+      <h1>the wild oasis website</h1>
+      <Link href="/cabins">cabins</Link>
+    </div>
+  );
 };
 
 export default Page;
