88b358e7b9f3a973ce351ade978fe6de31e2ef06
idknevo <nevoexists@gmail.com>
2025-06-28 01:59:19 +0300

    dynamic metadata

--- app/cabins/[cabinId]/page.tsx
+++ app/cabins/[cabinId]/page.tsx
@@ -1,24 +1,16 @@
 import { getCabin } from "@/app/_lib/data-service";
-import { CabinItem } from "@/app/_types";
 import { EyeSlashIcon, MapPinIcon, UsersIcon } from "@heroicons/react/24/solid";
 import { NextPage } from "next";
 import Image from "next/image";
 
-// PLACEHOLDER DATA
-const cabin = {
-  id: 89,
-  name: "001",
-  maxCapacity: 2,
-  regularPrice: 250,
-  discount: 0,
-  description:
-    "Discover the ultimate luxury getaway for couples in the cozy wooden cabin 001. Nestled in a picturesque forest, this stunning cabin offers a secluded and intimate retreat. Inside, enjoy modern high-quality wood interiors, a comfortable seating area, a fireplace and a fully-equipped kitchen. The plush king-size bed, dressed in fine linens guarantees a peaceful nights sleep. Relax in the spa-like shower and unwind on the private deck with hot tub.",
-  image:
-    "https://hxyuutkgtgcugaxwuhrc.supabase.co/storage/v1/object/public/cabin-images/cabin-001.jpg",
-};
-
 interface Props {
   params: Promise<{ cabinId: number }>;
+}
+
+export async function generateMetadata({ params }: Props) {
+  const { cabinId } = await params;
+  const { name } = await getCabin(cabinId);
+  return { title: `Cabin ${name}` };
 }
 
 const Page: NextPage<Props> = async ({ params }) => {
