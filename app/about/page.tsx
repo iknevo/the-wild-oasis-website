053cec715c8ea5f8bc951287decf8801cf9b3d6e
idknevo <nevoexists@gmail.com>
2025-07-05 03:46:31 +0300

    about chaching

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,16 +1,16 @@
+import { getCabins } from "@/app/_lib/data-service";
 import image1 from "@/public/about-1.jpg";
 import image2 from "@/public/about-2.jpg";
+import { NextPage } from "next";
+import Image from "next/image";
 
+export const validate = 86400; // 1 day
 export const metadata = {
   title: "About",
 };
 
-import { NextPage } from "next";
-import Image from "next/image";
-
-interface Props {}
-
-const Page: NextPage<Props> = ({}) => {
+export default async function Page() {
+  const cabins = await getCabins();
   return (
     <div className="grid grid-cols-5 gap-x-24 gap-y-32 text-lg items-center">
       <div className="col-span-3">
@@ -27,10 +27,10 @@ const Page: NextPage<Props> = ({}) => {
             simple pleasures with family.
           </p>
           <p>
-            Our 8 luxury cabins provide a cozy base, but the real freedom and
-            peace you'll find in the surrounding mountains. Wander through lush
-            forests, breathe in the fresh air, and watch the stars twinkle above
-            from the warmth of a campfire or your hot tub.
+            Our {cabins.length} luxury cabins provide a cozy base, but the real
+            freedom and peace you'll find in the surrounding mountains. Wander
+            through lush forests, breathe in the fresh air, and watch the stars
+            twinkle above from the warmth of a campfire or your hot tub.
           </p>
           <p>
             This is where memorable moments are made, surrounded by nature's
@@ -89,6 +89,4 @@ const Page: NextPage<Props> = ({}) => {
       </div>
     </div>
   );
-};
-
-export default Page;
+}
